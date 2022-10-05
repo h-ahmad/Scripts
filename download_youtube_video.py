8e16f36a65266820aee2dcd43e84832cb6ca2389
Hussain Ahmad <hamadni@yahoo.com>
2022-10-05 15:28:52 +0200

    Update download_youtube_video.py

--- download_youtube_video.py
+++ download_youtube_video.py
@@ -6,10 +6,8 @@ Created on Mon Oct  3 16:13:25 2022
 """
 
 # pip install pytube
-`
 from pytube import YouTube
 url = 'https://www.youtube.com/watch?v=gQ3IgGzrN9Q&ab_channel=FahadAbbasi'
 output_path = ''
 # YouTube(url).streams.first().download(output_path)     # lowest quality
 YouTube(url).streams.get_highest_resolution().download(output_path)       # highest quality
-`
