3527e8981788ac880033d2c5a11192d0a25fa090
Hussain Ahmad <hamadni@yahoo.com>
2022-10-26 21:20:01 +0200

    Add files via upload

--- divide_data_over_clients.py
+++ divide_data_over_clients.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 26 12:05:21 2022
+Created on Wed Oct 26 16:36:59 2022
 
 @author: hussain
 """
@@ -14,27 +14,43 @@ import csv
 
 def main():
     data_path = 'data/'
-    dataset_name = 'mnist'  # (cifar10, mnist) pickle file name
+    dataset_name = 'cifar10'  # (cifar10, mnist) pickle file name
     number_of_clients = 3
     with open(os.path.join(data_path, dataset_name+'.pkl'), 'rb') as file:
         data_store = pickle.load(file)
         print(data_store.keys())     
-        print('data: ', data_store['data'].shape)
-        print('label: ', data_store['label'].shape)
-    total_data_count = data_store['label'].shape[0]
+        print('X_train: ', data_store['X_train'].shape)
+        print('y_train: ', data_store['y_train'].shape)
+        print('X_test: ', data_store['X_test'].shape)
+        print('y_test: ', data_store['y_test'].shape)        
+    total_data_count = data_store['y_train'].shape[0]
     client_data = int(total_data_count/number_of_clients)
+    # save multiple train data for multiple clients given
     j = 0
     for i in range(total_data_count):
         if i % client_data == 0:
             j = j + 1
             os.makedirs(os.path.join(data_path, 'client'+str(j)+'/'), exist_ok = True)
             data_store_path = os.path.join(data_path, 'client'+str(j)+'/')   
-            csv_file = file = open(os.path.join(data_path, str(j)+'.csv'), 'w', newline='')
+            csv_file = open(os.path.join(data_path, str(j)+'.csv'), 'w', newline='')
             writer = csv.writer(csv_file)      
-#        image = data_store['data'][i]  # for numpy . i.e CIFAR10
-        image = data_store['data'][i].numpy()  # for tensor . i.e MNIST
+        image = data_store['X_train'][i]  # for numpy . i.e CIFAR10
+#        image = data_store['X_train'][i].numpy()  # for tensor . i.e MNIST
         cv2.imwrite(os.path.join(data_store_path, str(i)+'.png'), image)                
-        writer.writerow([str(i), data_store['label'][i]])
+        writer.writerow([str(i), data_store['y_train'][i]])
+    csv_file.close()
+    # save a single test data for all clients
+    csv_file = open(os.path.join(data_path, 'test.csv'), 'w', newline='')
+    writer = csv.writer(csv_file)     
+    os.makedirs(os.path.join(data_path, 'test'), exist_ok = True)
+    data_store_path = os.path.join(data_path, 'test')
+    csv_file = open(os.path.join(data_path, 'test.csv'), 'w', newline='')
+    writer = csv.writer(csv_file)    
+    for k in range(data_store['y_test'].shape[0]):
+        image = data_store['X_test'][k]  # for numpy . i.e CIFAR10
+#        image = data_store['X_test'][k].numpy()  # for tensor . i.e MNIST
+        cv2.imwrite(os.path.join(data_store_path, str(k)+'.png'), image)
+        writer.writerow([str(k), data_store['y_test'][k]])
     csv_file.close()
 
 if __name__ == '__main__':
