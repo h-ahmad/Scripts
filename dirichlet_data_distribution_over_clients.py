daec5c43df1ab752b0aa0e834b20cb8af0ef9328
Hussain Ahmad <hamadni@yahoo.com>
2022-11-07 15:28:45 +0100

    Update dirichlet_data_distribution_over_clients.py

--- dirichlet_data_distribution_over_clients.py
+++ dirichlet_data_distribution_over_clients.py
@@ -28,8 +28,8 @@ args = parser.parse_args()
 
 def cifar10():
     transform = transforms.Compose([transforms.ToTensor(),
-                                    # transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                                                         # std=[0.229, 0.224, 0.225])
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                                         std=[0.229, 0.224, 0.225])
                                     ])
     trainset = torchvision.datasets.CIFAR10(root=args.data_path,
                                           train=True , download=True, transform=transform)
