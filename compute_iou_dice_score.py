326287e909ab87b2e31bc145e1419dfd746fb916
Hussain Ahmad <hamadni@yahoo.com>
2024-03-04 07:22:36 -0800

    Update compute_iou_dice_score.py

--- compute_iou_dice_score.py
+++ compute_iou_dice_score.py
@@ -1,120 +1,90 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  2 15:22:37 2024
+Created on Mon Mar  4 11:20:22 2024
 
 @author: hussain
 """
 
 import argparse
 import os
-import csv
-import cv2
 import numpy as np
 import pandas as pd
+import cv2
 
-parser = argparse.ArgumentParser('Calculating IoU', add_help=False)
-parser.add_argument('--data_path', default='../datasets', type=str)
-parser.add_argument('--data_file_name', default='metadata_kf5.csv', type=str)
+parser = argparse.ArgumentParser('Calculating IoU/dice', add_help=False)
+parser.add_argument('--gt_mask_path', default='../../../../data/raabindata/Ground Truth/', type=str, help='root path to GT classes/images')
+parser.add_argument('--pred_mask_path', default='../MCTformer_plus/output/aml_mll_pbc_bmc/raabin/fold5/output_masks/', type=str, help='root path to predicted classes/images')
+parser.add_argument('--output_path', default='../MCTformer_plus/output/aml_mll_pbc_bmc/raabin/fold1/', type=str)
+parser.add_argument('--output_filename', default='iou_dice.csv', type=str)
 args = parser.parse_args()
 
-def read_data():
-    img_names = []
-    with open(os.path.join(args.data_path, args.data_file_name), 'r') as file:            
-        file_rows = csv.reader(file)
-        for i, row in enumerate(file_rows):
-            if i > 0 and row[3] == 'test':
-                img_path = row[1].split('../')[3] # original path without back slash
-                img_names.append(img_path)
-    return img_names
-    
-def get_mask_list(gt_mask_paths):
-    gt_mask_list, mctformer_mask_list, maskrcnn_mask_list = [], [], []
-    for i, gt_mask_path in enumerate(gt_mask_paths):
-        name = gt_mask_path.split('/')[-1] # img/mask name with extension
-        gt_mask = cv2.imread(os.path.join('../../../', gt_mask_path))
-        gt_mask = (gt_mask > 0).astype(np.uint8)
-        gt_mask_list.append(gt_mask)
-        
-        mctformer_mask_path = os.path.join('../MCTformer/output_masks', name.split('.')[0]+'.png')
-        if os.path.exists(mctformer_mask_path):
-            mctformer_mask = cv2.imread(mctformer_mask_path)
-            # mctformer_mask = cv2.cvtColor(mctformer_mask, cv2.COLOR_BGR2GRAY) 
-            mctformer_mask = (mctformer_mask > 0).astype(np.uint8)
-            mctformer_mask_list.append(mctformer_mask)
-        else:
-            mctformer_mask_list.append('')
-            
-        maskrcnn_mask_path = os.path.join('../maskrcnn/output_masks', name)
-        if os.path.exists(maskrcnn_mask_path):
-            maskrcnn_mask = cv2.imread(maskrcnn_mask_path)
-            # maskrcnn_mask = cv2.cvtColor(maskrcnn_mask, cv2.COLOR_BGR2GRAY) 
-            maskrcnn_mask = (maskrcnn_mask > 0).astype(np.uint8)
-            maskrcnn_mask_list.append(maskrcnn_mask)
-        else:
-            maskrcnn_mask_list.append('')
-        # if i == 1:
-            # break
-    return gt_mask_list, mctformer_mask_list, maskrcnn_mask_list
+class SegmentationEvaluation():
+    def __init__(self, gt_mask_path, pred_mask_path):
+        self.gt_mask_path = gt_mask_path
+        self.pred_mask_path = pred_mask_path
+        self.gt_mask_list, self.pred_mask_list = [], []
+        self.gt_masks, self.pred_masks = self.read_data()
+        self.iou_values, self.dice_values = [], []
 
-def calculate_iou(gt_mask, pred_mask):
-    # print('pred_mask: ', pred_mask.shape, 'min: ', pred_mask.min(), ' max: ', pred_mask.max())
-    tp = 0
-    fp = 0
-    fn = 0
-    for i in range(len(gt_mask)):
-        for j in range(len(gt_mask[0])):
-            if gt_mask[i][j] == 1 and pred_mask[i][j] == 1:
-                tp += 1
-            elif gt_mask[i][j] == 0 and pred_mask[i][j] == 1:
-                fp += 1
-            elif gt_mask[i][j] == 1 and pred_mask[i][j] == 0:
-                fn += 1
-    iou = tp / (tp + fp + fn)
-    return iou
+    def read_data(self):
+        check_list = []
+        for pred_cls_img in os.listdir(self.pred_mask_path):
+            cls_img_path = os.path.join(self.pred_mask_path, pred_cls_img)
+            if os.path.isdir(cls_img_path):
+                for pred_mask in os.listdir(cls_img_path):
+                    read_pred_mask = cv2.imread(os.path.join(cls_img_path, pred_mask))
+                    self.pred_mask_list.append(read_pred_mask)
+                    pred_mask_name, pred_extension = os.path.splitext(pred_mask)
+                    check_list.append(pred_mask_name)
+            else:
+                read_pred_mask = cv2.imread(cls_img_path)
+                self.pred_mask_list.append(read_pred_mask)
+                pred_mask_name, pred_extension = os.path.splitext(pred_cls_img)
+                check_list.append(pred_mask_name)                
+                           
+        for gt_cls_img in os.listdir(self.gt_mask_path):
+            cls_img_path = os.path.join(self.gt_mask_path, gt_cls_img)
+            if os.path.isdir(cls_img_path):
+                for gt_mask in os.listdir(cls_img_path):                    
+                    gt_mask_name, gt_extension = os.path.splitext(gt_mask)
+                    if gt_mask_name in check_list:
+                        read_gt_mask = cv2.imread(os.path.join(cls_img_path, gt_mask))
+                        self.gt_mask_list.append(read_gt_mask)
+            else:
+                gt_mask_name, gt_extension = os.path.splitext(gt_cls_img)
+                if gt_mask_name in check_list:
+                    read_gt_mask = cv2.imread(cls_img_path)
+                    self.gt_mask_list.append(read_gt_mask)                                
+        return self.gt_mask_list, self.pred_mask_list       
 
-def compute_iou(mask1, mask2):
-    intersection = np.logical_and(mask1, mask2)
-    union = np.logical_or(mask1, mask2)
-    iou = np.sum(intersection) / np.sum(union)
-    return iou
+    def compute_iou(self, mask1, mask2):
+        intersection = np.logical_and(mask1, mask2)
+        union = np.logical_or(mask1, mask2)
+        iou = np.sum(intersection) / np.sum(union)
+        return iou
 
-def dice_coefficient(mask1, mask2):
-    intersection = np.sum(np.logical_and(mask1, mask2))
-    total_pixels_mask1 = np.sum(mask1)
-    total_pixels_mask2 = np.sum(mask2)
-    dice = (2.0 * intersection) / (total_pixels_mask1 + total_pixels_mask2)
-    return dice
+    def dice_coefficient(self, mask1, mask2):
+        intersection = np.sum(np.logical_and(mask1, mask2))
+        total_pixels_mask1 = np.sum(mask1)
+        total_pixels_mask2 = np.sum(mask2)
+        dice = (2.0 * intersection) / (total_pixels_mask1 + total_pixels_mask2)
+        return dice
 
-def compute_iou_dice_between_lists(mask_list1, mask_list2):
-    iou_values, dice_values = [], []
-    for i, mask1 in enumerate(mask_list1):
-        mask2 = mask_list2[i]
-        if mask2 == '':
-            iou = 0.0
-            dice = 0.0
-        else:
-            iou = compute_iou(mask1, mask2)
-            # iou = calculate_iou(mask1, mask2)
-            dice = dice_coefficient(mask1, mask2)
-        iou_values.append(iou)
-        dice_values.append(dice)
-    return iou_values, dice_values
+    def compute_iou_dice(self):
+        for i, mask1 in enumerate(self.gt_masks):
+            mask2 = self.pred_masks[i]            
+            iou = self.compute_iou(mask1, mask2)
+            dice = self.dice_coefficient(mask1, mask2)
+            self.iou_values.append(iou)
+            self.dice_values.append(dice)
+        return self.iou_values, self.dice_values         
 
 if __name__ == '__main__':
-    gt_mask_paths = read_data()
-    gt_mask_list, mctformer_mask_list, maskrcnn_mask_list = get_mask_list(gt_mask_paths)
-    mctformer_iou, mctformer_dice = compute_iou_dice_between_lists(gt_mask_list, mctformer_mask_list)
-    maskrcnn_iou, maskrcnn_dice = compute_iou_dice_between_lists(gt_mask_list, maskrcnn_mask_list)
-    mctformer_iou_mean = np.mean(np.array(mctformer_iou))
-    mctformer_dice_mean = np.mean(np.array(mctformer_dice))
-    maskrcnn_iou_mean = np.mean(np.array(maskrcnn_iou))
-    maskrcnn_dice_mean = np.mean(np.array(maskrcnn_dice))
-    print('mctformer_iou_mean: ', mctformer_iou_mean, ' maskrcnn_iou_mean: ', maskrcnn_iou_mean, 
-          ' mctformer_dice_mean: ', mctformer_dice_mean, ' maskrcnn_dice_mean: ', maskrcnn_dice_mean)
-    data = {'mactformer_iou': mctformer_iou, 'maskrcnn_iou': maskrcnn_iou, 
-            'mctformer_iou_mean': mctformer_iou_mean, 'maskrcnn_iou_mean': maskrcnn_iou_mean, 
-            'mctformer_dice': mctformer_dice, 'maskrcnn_dice': maskrcnn_dice,
-            'mctformer_dice_mean': mctformer_dice_mean, 'maskrcnn_dice_mean': maskrcnn_dice_mean}
-    df = pd.DataFrame(data)
-    df.to_csv('iou.csv')
+    seg_eval = SegmentationEvaluation(args.gt_mask_path, args.pred_mask_path)
+    iou, dice = seg_eval.compute_iou_dice()
+    mean_iou = np.mean(np.array(iou))
+    mean_dice = np.mean(np.array(dice))
+    print('mean_iou: ', mean_iou, ', mean_dice: ', mean_dice)
+    data = {'mean_iou':mean_iou, 'mean_dice':mean_dice}
+    # df = pd.DataFrame(data)    
+    # df.to_csv(os.path.join(args.output_path, args.output_filename))
